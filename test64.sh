42dc1a068097b19bafbb48bd2815cdc19cd43b2d
RafLaf <ralafarg@gmail.com>
2022-03-02 17:30:08 +0100

    2 ways

--- test64.sh
+++ test64.sh
@@ -1,14 +1,14 @@
 
   
-python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features /users/local/vincent/full1  --wandb brain-imt
+python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features /users/local/vincent/full1  --wandb brain-imt --n-ways 2
 
 
-python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features /users/local/vincent/f11  --wandb brain-imt  --nb-of-rm 1 
-
-
-
-python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features /users/local/vincent/full1  --wandb brain-imt --forced-class
+python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features /users/local/vincent/f11  --wandb brain-imt  --nb-of-rm 1 --n-ways 2
 
 
 
-python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features /users/local/vincent/f11  --wandb brain-imt  --nb-of-rm 1 --forced-class
+python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features /users/local/vincent/full1  --wandb brain-imt --forced-class --n-ways 2
+
+
+
+python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features /users/local/vincent/f11  --wandb brain-imt  --nb-of-rm 1 --forced-class --n-ways 2
